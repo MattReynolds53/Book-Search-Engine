7ef93fb85d33ae55a5bc39d7ca8a4402797453f1
Matt Reynolds <mattreynolds53@gmail.com>
2022-01-28 21:56:30 -0600

    Added useMutation hook to implement the loginUser function into the LoginForm.js file.

--- Develop/client/src/components/LoginForm.js
+++ Develop/client/src/components/LoginForm.js
@@ -1,8 +1,8 @@
 // see SignupForm.js for comments
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
-
-import { loginUser } from '../utils/API';
+import { useMutation } from '@apollo/client';
+// import { loginUser } from '../utils/API';
 import Auth from '../utils/auth';
 
 const LoginForm = () => {
@@ -26,7 +26,8 @@ const LoginForm = () => {
     }
 
     try {
-      const response = await loginUser(userFormData);
+      const [loginUser, { error }] = useMutation(LOGIN_USER);
+      // const response = await loginUser(userFormData);
 
       if (!response.ok) {
         throw new Error('something went wrong!');
